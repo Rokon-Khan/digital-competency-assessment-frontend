36b24936680eac41ea8e271e760dcd24a2660679
Rokon Khan <rokonjust@gmail.com>
2025-08-09 14:06:29 +0600

    Digital competency assesment Project React With TypeScript Set-Up Done

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <main className="flex flex-col min-h-screen justify-center items-center space-y-4">
+        <h1 className="text-3xl text-center">
+          Digital Competency Assesment Frontend project
+        </h1>
+        <div>
+          <Button>View Detail</Button>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
